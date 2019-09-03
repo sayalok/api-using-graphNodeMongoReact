a414e47d34e33f950784750507c7baa25ad9fb77
sayalok <sayalok@yahoo.com>
2019-09-03 22:32:52 +0600

    add form in modal

--- frontend/src/pages/Product.js
+++ frontend/src/pages/Product.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
+	import React, { Component } from 'react';
 
 import Backdrop from '../inc/Backdrop/Backdrop';
 import ModalForm from '../inc/ModalForm/ModalForm';
 
 class Product extends Component {
+	constructor(props) {
+    	super(props);
+		this.titleRef = React.createRef();
+		this.priceRef = React.createRef();
+		this.dateRef = React.createRef();
+		this.descRef = React.createRef();
+	}
+	modalConfirmHandler = e => {
+		e.preventDefault();
+		const title = this.titleRef.current.value;
+		const price = this.priceRef.current.value;
+		const date = this.dateRef.current.value;
+		const desc = this.descRef.current.value;
+	}
     render() {
         return (
             <React.Fragment>
@@ -14,6 +28,36 @@ class Product extends Component {
 					</button>
 				</div>
 				<ModalForm title="Add Product">
+					<form onSubmit={this.modalConfirmHandler}>
+						<div className="form-group">
+							<label>Enter Title</label>
+							<input 
+								type="text" className="form-control" 
+								id="title" aria-describedby="titleHelp" 
+								placeholder="Enter title"
+								ref={this.titleRef}/>
+						</div>
+						<div className="form-group">
+							<label>Enter Price</label>
+							<input 
+								type="text" className="form-control" 
+								id="Price" aria-describedby="PriceHelp" 
+								placeholder="Enter Price" ref={this.priceRef}/>
+						</div>
+						<div className="form-group">
+							<label>Enter Date</label>
+							<input 
+								type="date" className="form-control" id="date" 
+								aria-describedby="dateHelp" ref={this.dateRef}/>
+						</div>
+						<div className="form-group">
+							<label>Enter Description</label>
+							<textarea 
+								className="form-control" id="desc" rows="3"
+								ref={this.descRef}></textarea>
+						</div>
+		        		<button type="submit" className="btn btn-primary">Save</button>
+					</form>
 				</ModalForm>
             </React.Fragment>
         );
